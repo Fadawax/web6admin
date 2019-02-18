4a16629db950b022f6f2a0518e24d503a401382c
brobin <brobin@ALICS051>
2019-02-18 14:42:32 +0100

    structure pages

---
+++ accueil.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+
+<html>
+
+ <head>
+ 
+  <meta charset="utf-8" />
+  <title>Web6Admin</title>
+  
+ </head>
+
+ <body>
+ 
+  <header>
+  
+  </header>        
+  
+  <section>
+  
+  </section>      
+  
+  <footer>
+  
+  </footer>
+        
+ </body>
+ 
+</html>
