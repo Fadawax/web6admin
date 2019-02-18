535c5ada8fe69d003ca5bcca3e3ba05f1cb2c3c1
brobin <brobin@ALICS051>
2019-02-18 13:11:53 +0100

    mise en page

--- index.php
+++ index.php
@@ -1,3 +1,28 @@
-<php
-echo "Hello World"
-?php>
+<!DOCTYPE html>
+
+<html>
+
+ <head>
+ 
+  <meta charset="utf-8" />
+  <title>Web6Admin</title>
+  
+ </head>
+
+ <body>
+ 
+  <header>
+  
+  </header>        
+  
+  <section>
+  
+  </section>      
+  
+  <footer>
+  
+  </footer>
+        
+ </body>
+ 
+</html>
